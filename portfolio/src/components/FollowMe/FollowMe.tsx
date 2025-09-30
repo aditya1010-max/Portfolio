71b2b06d3e39643cbb57a985f874e66542db5cf3
aditya1010-max <rauta2813@gmail.com>
2025-10-01 02:00:49 +0530

    small css tweaks

--- portfolio/src/components/FollowMe/FollowMe.tsx
+++ portfolio/src/components/FollowMe/FollowMe.tsx
@@ -5,16 +5,16 @@ export default function FollowMe() {
     <div className="followme">
       <h1>#Socials <div className="horizontal-line"></div></h1>
 
-      <a href="https://github.com/aditya1010-max/">
+      <a href="https://github.com/aditya1010-max/" target="_blank" rel="noopener noreferrer">
         <img src="github.png" alt="GitHub" style={{height: "45px", width: "45px"}}/>
       </a>
-      <a href="https://www.linkedin.com/in/aditya-raut-35b1672a5/">
+      <a href="https://www.linkedin.com/in/aditya-raut-35b1672a5/" target="_blank" rel="noopener noreferrer">
         <img src="icons8-linkedin-48.png" alt="linkedin" />
       </a>      
-      <a href="https://x.com/Raut100x">
+      <a href="https://x.com/Raut100x" target="_blank" rel="noopener noreferrer">
         <img src="icons8-twitter-50.png" alt="Twitter" />
       </a>  
-      <a href="https://leetcode.com/u/AdiiiTya_1010/">
+      <a href="https://leetcode.com/u/AdiiiTya_1010/" target="_blank" rel="noopener noreferrer">
         <img src="lc2.png" alt="leetcode" style={{height: "45px", width: "45px"}} />
       </a>      
     </div>
--- portfolio/src/components/Github-Stats/GhStats.tsx
+++ portfolio/src/components/Github-Stats/GhStats.tsx
@@ -4,10 +4,12 @@ export default function GhStats() {
   return (
     <div className="ghstats">
       <h2>GitHub Stats</h2>
-      <img
-        src="https://github-readme-stats.vercel.app/api?username=aditya1010-max&show_icons=true&theme=radical"
-        alt="GitHub Stats"
-      />
+        <a href="https://github.com/aditya1010-max/" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://github-readme-stats.vercel.app/api?username=aditya1010-max&show_icons=true&theme=radical"
+            alt="GitHub Stats"
+          />
+        </a>
     </div>
   );
 }
